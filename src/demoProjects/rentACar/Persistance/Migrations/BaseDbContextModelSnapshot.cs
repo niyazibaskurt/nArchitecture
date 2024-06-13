2d6c719eaf50ae7d9604b5f4d13b54b7362dc7f4
niyazi.baskurt@spk.gov.tr <niyazi.baskurt@spk.gov.tr>
2024-06-13 15:31:45 +0300

    nArchitecture

--- src/demoProjects/rentACar/Persistance/Migrations/BaseDbContextModelSnapshot.cs
+++ src/demoProjects/rentACar/Persistance/Migrations/BaseDbContextModelSnapshot.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Contexts;
 
@@ -11,10 +10,9 @@ using Persistence.Contexts;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(BaseDbContext))]
-    [Migration("20220815205354_Init")]
-    partial class Init
+    partial class BaseDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
